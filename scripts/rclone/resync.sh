ddf04a19f54b295ec09bfc99914562bdccd75d12
AzPepoze <pladumguy@gmail.com>
2025-07-04 21:11:45 +0700

    ✨ Improves touchpad and rclone scripts
    Improves touchpad scrolling experience with a smaller scroll factor.
    
    Updates rclone scripts to use home directory relative paths for log files, improving portability.

--- scripts/rclone/resync.sh
+++ scripts/rclone/resync.sh
@@ -2,15 +2,14 @@
 # Performs a full resync with rclone bisync.
 # The --resync flag forces a full comparison of all files.
 
-RCLONE_LOG_FILE="/home/azpepoze/arch-setup/scripts/rclone/rclone.log"
+RCLONE_LOG_FILE="~/arch-setup/scripts/rclone/rclone.log"
 
 rclone bisync gdrive: ~/GoogleDrive \
-    --transfers=24 \
-    --checkers=48 \
-    --drive-chunk-size=64M \
-    --fast-list \
-    --progress \
-    --drive-acknowledge-abuse \
-    --resync \
-    --log-file="$RCLONE_LOG_FILE"
-
+     --transfers=24 \
+     --checkers=48 \
+     --drive-chunk-size=64M \
+     --fast-list \
+     --progress \
+     --drive-acknowledge-abuse \
+     --resync \
+     --log-file="$RCLONE_LOG_FILE"
--- scripts/rclone/sync.sh
+++ scripts/rclone/sync.sh
@@ -2,14 +2,13 @@
 # Performs a normal rclone bisync.
 # This relies on listings from the last run for faster execution.
 
-RCLONE_LOG_FILE="/home/azpepoze/arch-setup/scripts/rclone/rclone.log"
+RCLONE_LOG_FILE="~/arch-setup/scripts/rclone/rclone.log"
 
 rclone bisync gdrive: ~/GoogleDrive \
-    --transfers=24 \
-    --checkers=48 \
-    --drive-chunk-size=64M \
-    --fast-list \
-    --progress \
-    --drive-acknowledge-abuse \
-    --log-file="$RCLONE_LOG_FILE"
-
+     --transfers=24 \
+     --checkers=48 \
+     --drive-chunk-size=64M \
+     --fast-list \
+     --progress \
+     --drive-acknowledge-abuse \
+     --log-file="$RCLONE_LOG_FILE"
