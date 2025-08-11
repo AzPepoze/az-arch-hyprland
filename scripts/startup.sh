3065a0b9a9cfc848e3cb1057a8d379341d32d679
AzPepoze <pladumguy@gmail.com>
2025-08-12 03:53:54 +0700

    update notification center

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -48,6 +48,7 @@ launch_messenger() {
 # Background Services
 #-------------------------------------------------------
 launch_app "rclone sync" "bash $HOME/az-arch-hyprland/scripts/rclone/sync.sh &"
+bash $HOME/az-arch-hyprland/scripts/utils/notification_logger.sh &
 
 #-------------------------------------------------------
 # Startup Programs
