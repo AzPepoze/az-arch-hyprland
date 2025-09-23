3f56d0691f93afa9e636654154595f66f3e4ab3f
AzPepoze <pladumguy@gmail.com>
2025-09-24 04:54:56 +0700

    ✨ Add check for fish_prompt_pwd_dir_length initialization

--- dots/base/home/config/fish/config.fish
+++ dots/base/home/config/fish/config.fish
@@ -1,3 +1,7 @@
+if not set -q fish_prompt_pwd_dir_length
+    set fish_prompt_pwd_dir_length 0
+end
+
 function fish_prompt -d "Write out the prompt"
     # This shows up as USER@HOST /home/user/ >, with the directory colored
     # $USER and $hostname are set by fish, so you can just use them
